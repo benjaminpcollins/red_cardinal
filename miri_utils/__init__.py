18cee3bc3a9983dc75ab43694cc5bcc8713e3634
Benjamin Philip Collins <benjamin.p.collins@icloud.com>
2025-06-14 19:06:47 +0200

    Prompted CLAUDE to write a function that analyses two photometric FITS tables containing photometric data WITHOUT the aperture corrections. It compares and contrasts the different fluxes in multiple parameter dimensions and shows them on a neat and informative 4x4 summary plot. It also creates a text file that stores the most important data for a quick overview.

--- miri_utils/__init__.py
+++ miri_utils/__init__.py
@@ -3,5 +3,5 @@
 from .cutout_tools import load_cutout, produce_cutouts
 from .astrometry_utils import compute_centroid, save_alignment_figure, compute_offset, write_offset_stats, visualise_offsets, shift_miri_fits, get_mean_stats
 from .rotation_utils import calculate_angle, rotate_exp, expand_fits
-from .photometry_tools import adjust_aperture, estimate_background, visualise_background, get_psf, get_aperture_params, calculate_aperture_correction, measure_flux, perform_photometry, combine_figures, create_fits_table_from_csv, combine_filter_csv_to_fits
+from .photometry_tools import adjust_aperture, estimate_background, visualise_background, get_psf, get_aperture_params, calculate_aperture_correction, measure_flux, perform_photometry, combine_figures, create_fits_table_from_csv, combine_filter_csv_to_fits, compare_aperture_statistics
 from .stamp_maker import resample_nircam, normalise_image, preprocess_fits_image, make_stamp
