135cdc354a00158564e98f2f3a56cb4f6c849bac
Benjamin Philip Collins <benjamin.p.collins@icloud.com>
2025-06-22 11:43:54 +0200

    Added a new function to astrometry_utils that creates polar scatter plots of the astrometric offsets.

--- miri_utils/__init__.py
+++ miri_utils/__init__.py
@@ -1,9 +1,9 @@
 # __init__.py for the miri_utils package
 
 from .cutout_tools import load_cutout, produce_cutouts
-from .astrometry_utils import compute_centroid, save_alignment_figure, compute_offset, write_offset_stats, visualise_offsets, shift_miri_fits, get_mean_stats
+from .astrometry_utils import compute_centroid, save_alignment_figure, compute_offset, write_offset_stats, visualise_offsets, shift_miri_fits, get_mean_stats, \
+    plot_astrometric_offsets
 from .rotation_utils import calculate_angle, rotate_exp, expand_fits
 from .photometry_tools import adjust_aperture, estimate_background, visualise_background, get_psf, get_aperture_params, calculate_aperture_correction, measure_flux, \
     perform_photometry, combine_figures, create_fits_table_from_csv, combine_filter_csv_to_fits, compare_aperture_statistics
 from .stamp_maker import resample_nircam, normalise_image, preprocess_fits_image, make_stamp
-from .prospector_utils import get_zred, load_hst_nircam_phot, load_miri_phot, reconstruct_model_from_h5, plot_prospector_with_data, example_usage, plot_multiple_objects_fixed 
