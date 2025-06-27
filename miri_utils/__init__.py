93f1bc37bd027e42b819300046634178ba57c47b
Benjamin Philip Collins <benjamin.p.collins@icloud.com>
2025-06-27 19:57:35 +0200

    Restructured the way the FITS files are rotated. Instead of taking the whole mosaic and rotating it after expansion, only the cutouts are rotated now and cropped to a size of 3x3 arcsec.

--- miri_utils/__init__.py
+++ miri_utils/__init__.py
@@ -1,9 +1,8 @@
 # __init__.py for the miri_utils package
 
-from .cutout_tools import load_cutout, produce_cutouts
+from .cutout_tools import load_cutout, produce_cutouts, calculate_angle, rotate_cutouts
 from .astrometry_utils import compute_centroid, save_alignment_figure, compute_offset, write_offset_stats, visualise_offsets, shift_miri_fits, get_mean_stats, \
     plot_astrometric_offsets
-from .rotation_utils import calculate_angle, rotate_exp, expand_fits
 from .photometry_tools import adjust_aperture, estimate_background, get_psf, get_aperture_params, calculate_aperture_correction, measure_flux, \
     perform_photometry, create_fits_table_from_csv, compare_aperture_statistics, galaxy_statistics, plot_galaxy_filter_matrix, write_galaxy_stats, \
         save_vis, load_vis, create_mosaics
