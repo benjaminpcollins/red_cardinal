ca5dc790aeb9dabdb66902921be8494cfadf50b8
Benjamin Philip Collins <benjamin.p.collins@icloud.com>
2025-11-21 12:55:55 +0100

    Remove pycache files from version control

--- prospector_utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# init for the prospector utils package
-
-from .plotting import reconstruct, plot_photometry, load_and_display, plot_transmission_curves, create_hist, \
-    plot_main_sequence, plot_mass_vs_redshift, plot_nsigma_vs_params, plot_extremes, show_fits_cutout, show_h5_cutout, show_png
-from .params import get_MAP, build_obs, build_model
-from .analysis import predict_phot, compute_residuals, get_model_photometry, get_galaxy_properties, get_extremes
